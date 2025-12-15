cd2c27e44e9e5673fb7b5453ad3f4a1b1916bf7c
jonathonrobertsf <jonathon@flyhighmedia.co.uk>
2025-12-15 10:03:49 +0000

    Create RootLayout component for app layout

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <title>Lighthouse Lab Metrics</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
